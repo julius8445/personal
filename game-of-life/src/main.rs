53c1111d1bcc27b352a1fda4202ca9dfa977c655
julius8445 <91995100+julius8445@users.noreply.github.com>
2023-06-20 18:58:02 -0400

    Add files via upload

--- game-of-life/src/main.rs
+++ game-of-life/src/main.rs
@@ -19,6 +19,17 @@ macro_rules! p {
 
 const BOARD_SIZE: usize = 50;
 
+const DIRECTIONS: &'static [Direction] = &[
+    Direction::N,
+    Direction::NE,
+    Direction::E,
+    Direction::SE,
+    Direction::S,
+    Direction::SW,
+    Direction::W,
+    Direction::NW,
+];
+
 const DIRECTION_DELTAS: &'static [(isize, isize)] = &[
     ( 1, 0),
     ( 1, 1),
@@ -31,9 +42,51 @@ const DIRECTION_DELTAS: &'static [(isize, isize)] = &[
 ];
 
 #[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Cell {
+    Dead  = 0,
+    Alive = 1,
+}
+
+impl Cell {
+    pub fn is_alive(&self) -> bool {
+        *self == Cell::Alive
+    }
+
+    pub fn is_dead(&self) -> bool {
+        !self.is_alive()
+    }
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Direction {
+    N  = 0,
+    NE = 1,
+    E  = 2,
+    SE = 3,
+    S  = 4,
+    SW = 5,
+    W  = 6,
+    NW = 7,
+}
+
+
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub struct Point {
-    row: usize,
     col: usize,
+    row: usize,
+}
+
+impl Point {
+    pub fn get_neighbor(&self, d: &Direction) -> Option<Point> {
+        let (Some(r), Some(c)) = (
+            self.row.checked_add_signed(DIRECTION_DELTAS[*d as usize].0),
+            self.col.checked_add_signed(DIRECTION_DELTAS[*d as usize].1),
+        ) else {
+            return None;
+        };
+
+        Some(p!(c, r))
+    }
 }
 
 impl Display for Point {
@@ -42,17 +95,6 @@ impl Display for Point {
     }
 }
 
-#[derive(Debug, PartialEq, Clone, Copy)]
-pub enum Cell {
-    Dead,
-    Alive,
-}
-
-impl Cell {
-    pub fn is_alive(&self) -> bool {
-        *self == Cell::Alive
-    }
-}
 
 #[derive(Debug)]
 pub struct GameState {
@@ -72,8 +114,33 @@ impl GameState {
     }
 
     pub fn update(&mut self) {
-        let _cells = self.cells
-                        .elements_row_major_iter();
+        // Alive
+        //    * 2 >  live_neighbors : Dies
+        //    * 3 >= live_neighbors >= 2 : Survives
+        //    * live_neighbors > 3: Dies
+        // Dead
+        //    * live_neighbors == 3 : Alive
+
+    }
+
+    pub fn alive_neighbor_count(&self, p: Point) -> usize {
+        let get_neighbor = |d: &Direction| Point::get_neighbor(&p, d);
+        let get_cell = |p: Point| GameState::get_cell(self, p);
+        let is_alive = |cell: &&Cell| Cell::is_alive(cell);
+
+        DIRECTIONS
+            .into_iter()
+            .filter_map(get_neighbor)
+            .filter_map(get_cell)
+            .filter(is_alive)
+            .count()
+    }
+
+    pub fn alive_cell_count(&self) -> usize {
+        self.cells
+            .elements_row_major_iter()
+            .filter(|cell| Cell::is_alive(cell))
+            .count()
     }
 
     pub fn get_cell(&self, p: Point) -> Option<&Cell> {
@@ -83,53 +150,6 @@ impl GameState {
     pub fn set_cell(&mut self, p: Point, value: Cell) {
         self.cells.set(p.row, p.col, value).unwrap();
     }
-
-    pub fn alive_neighbor_count(&self, p: Point) -> usize {
-        let mut result = 0;
-
-        for delta in DIRECTION_DELTAS {
-            let r = p.row.checked_add_signed(delta.0);
-            let c = p.col.checked_add_signed(delta.1);
-
-            let (Some(c), Some(r)) = (c, r) else {
-                continue;
-            };
-            
-            if let Some(Cell::Alive) = self.get_cell(p!(c, r)) {
-                result += 1;
-            };
-        }
-
-        result
-    }
-
-    pub fn alive_neighbor_count_iter(&self, p: Point) -> usize {
-        DIRECTION_DELTAS
-            .into_iter()
-            .filter_map(|delta| {
-                let (Some(r),Some(c)) = (
-                    p.row.checked_add_signed(delta.0),
-                    p.col.checked_add_signed(delta.1)
-                ) else {
-                    return None;
-                };
-
-                Some(p!(c, r))
-            })
-            .filter_map(|point| self.get_cell(point))
-            .filter(|x| x.is_alive())
-            .count()
-            //.for_each(|v| println!("{v:?}"));
-
-    }
-
-    pub fn alive_cell_count(&self) -> usize {
-        self.cells
-            .elements_row_major_iter()
-            .filter(|c| **c == Cell::Alive)
-            .count()
-    }
-
 
     pub fn width(&self) -> usize {
         self.width
@@ -147,14 +167,12 @@ fn main() {
     game.set_cell(p!(0,1), Cell::Alive);
     game.set_cell(p!(1,1), Cell::Alive);
     game.set_cell(p!(1,0), Cell::Alive);
+    game.set_cell(p!(2,0), Cell::Alive);
+    game.set_cell(p!(2,1), Cell::Alive);
 
-    let nc_1  = game.alive_neighbor_count(p!(0, 0));
-    let nc_1a = game.alive_neighbor_count(p!(1, 1));
-    let nc_2 = game.alive_neighbor_count_iter(p!(0,0));
-    let nc_2a = game.alive_neighbor_count_iter(p!(1, 1));
+    let nc_1a  = game.alive_neighbor_count(p!(0, 0));
+    let nc_1b = game.alive_neighbor_count(p!(1, 1));
 
-    dbg!(&nc_1);
-    dbg!(&nc_2);
     dbg!(&nc_1a);
-    dbg!(&nc_2a);
+    dbg!(&nc_1b);
 }
